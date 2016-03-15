133b3d13f1de0485dd8782dd940ce2e5c3ea6f35
John Krauss <irving.krauss@gmail.com>
2016-03-15 08:19:48 -0400

    handle whitespace inf ilenames

--- geocode.sh
+++ geocode.sh
@@ -2,6 +2,6 @@
 
 print_header=1
 for infile in txt/**/*.txt; do
-  python geocode.py $print_header $infile
+  python geocode.py $print_header "$infile"
   print_header=0
 done
--- ocr.sh
+++ ocr.sh
@@ -2,6 +2,6 @@
 
 cd tiffs
 for tiff in **/*.tiff; do
-  mkdir -p ../txt/$(dirname $tiff)
-  tesseract $tiff ../txt/$(dirname $tiff)/$(basename $tiff .tiff)
+  mkdir -p "../txt/$(dirname $tiff)"
+  tesseract "$tiff" "../txt/$(dirname $tiff)/$(basename $tiff .tiff)"
 done
